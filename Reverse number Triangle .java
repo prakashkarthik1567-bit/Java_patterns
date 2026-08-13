170a7321eddf6f3abb32be2159b6f135bd865aaf
Vignesh V <prakashkarthik1567@gmail.com>
2026-08-13 12:30:26 +0530

    Add Reverse Number Triangle Java program

---
+++ Reverse number Triangle .java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 8;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print(j+" ");
+            }
+         System.out.println();
+        }
+    }
+}
